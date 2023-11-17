77aeaaf9dfdebbb804ca214ddc3df19973fdd6ee
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-17 14:29:23 -0500

    add three fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import { Koulen } from "next/font/google";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      koulen: ["var(--font-koulen)"],
+      dawning: ["var(--font-dawning)"], // display (script) font
+      rubik: ["var(--font-rubik)"], // secondary display font
+    },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-koulen)"], // default font
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
