3e8dc2d95b286f90e861d7eae57b33aebeda69b8
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-21 16:31:54 -0500

    add hover transition to nav links

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -4,13 +4,22 @@ const Navbar = () => {
   return (
     <>
       <div className="border-4 border-indigo-500  text-base sm:text-5xl ">
-        <Link className="m-4 hover:opacity-80" href="/bio">
+        <Link
+          className="m-4 hover:opacity-80 transition duration-400 ease-in-out"
+          href="/bio"
+        >
           BIO
         </Link>
-        <Link className="m-4 hover:opacity-80" href="/video">
+        <Link
+          className="m-4 hover:opacity-80 transition duration-400 ease-in-out"
+          href="/video"
+        >
           Video
         </Link>
-        <Link className="m-4 hover:opacity-80" href="/contact">
+        <Link
+          className="m-4 hover:opacity-80 transition duration-400 ease-in-out"
+          href="/contact"
+        >
           Contact
         </Link>
       </div>
