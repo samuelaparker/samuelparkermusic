4c5022df778efdd2b4c228c6c5258b2859889be7
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-22 14:44:54 -0500

    add styling to bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -2,30 +2,30 @@ import Link from "next/link";
 
 export default function Bio() {
   return (
-    <div className="min-h-[100svh]">
-      <Link href="/" className="font-dawning text-2xl">
+    <div className="min-h-[100svh] mx-4 mt-4">
+      <Link href="/" className="font-dawning text-4xl ">
         Samuel Parker
       </Link>
-      <div>
-        <p>
+      <div className="mt-4 sm:mt-8 max-w-3xl">
+        <p className="mt-2 sm:text-2xl">
           From Maine&apos;s serene coast to the busy streets of NYC and now
           adding rhythm to Richmond, VA, Sam Parker&apos;s journey through jazz
           is a fusion of artistry and experience.
         </p>
-        <p>
-          Under the mentorship of Andy LaVerne, (a student of Bill Evans), Sam
+        <p className="mt-2 sm:text-2xl">
+          Under the mentorship of Andy LaVerne, ( a student of Bill Evans ), Sam
           refined his craft at the esteemed Hartt School of Music. Sam&apos;s
           piano instantly enhances an event with his knowledge of the American
           songbook and over a decade of experience playing jazz piano at clubs
           and parties.
         </p>
-        <p>
+        <p className="mt-2 sm:text-2xl">
           Sams forte? Reading the vibe. His music isn&apos;t just background
           noise; it&apos;s an atmosphere enhancer, tailored to the energy of
           each event. His sensitivity and intuition transform gatherings into
           unforgettable experiences, where sophistication meets spontaneity.
         </p>
-        <p>
+        <p className="mt-2 sm:text-2xl">
           For an event where taste, artistry, and an unbeatable vibe collide,
           invite Sam Parker to curate an experience that lingers.
         </p>
