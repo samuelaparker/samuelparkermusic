1e5123ee69dfa14a32a562a24260435813000d57
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-30 19:34:24 -0500

    add react-iframe todo: style

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import Iframe from "react-iframe";
 //https://github.com/tjallingt/react-youtube
-import YouTube, { YouTubeProps } from "react-youtube";
 
 export default function Example() {
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  };
+  // const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+  //   // access to player in all event handlers via event.target
+  //   event.target.pauseVideo();
+  // };
 
-  const opts: YouTubeProps["opts"] = {
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  };
+  // const opts: YouTubeProps["opts"] = {
+  //   playerVars: {
+  //     // https://developers.google.com/youtube/player_parameters
+  //     autoplay: 0,
+  //   },
+  // };
 
   return (
     <div className="mx-4 mt-4">
@@ -23,21 +23,30 @@ export default function Example() {
         Samuel Parker
       </Link>
       <div className="flex flex-wrap lg:flex-row flex-col pb-20">
-        <div className="pt-8 pr-8 aspect-w-16 aspect-h-9">
-          <YouTube
+        <div className="aspect-w-16 aspect-h-9">
+          <Iframe
+            url="https://www.youtube.com/embed/WyXFfKYbtQU"
+            //https://www.youtube.com/watch?v=fIFMCjsqtN8
+
+            id=""
+            className=""
+            display="block"
+            position="relative"
+          />
+          {/* <YouTube
             className=""
             videoId="fIFMCjsqtN8"
             opts={opts}
             onReady={onPlayerReady}
           />
         </div>
-        <div className="pt-8 aspect-w-16 aspect-h-9">
+        <div className="aspect-w-16 aspect-h-9">
           <YouTube
             className=""
             videoId="fIFMCjsqtN8"
             opts={opts}
             onReady={onPlayerReady}
-          />
+          /> */}
         </div>
       </div>
     </div>
