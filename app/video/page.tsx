701da862477cfc6a7362a0a2dc548bc1d20d070a
Samuel Parker <samuel.a.parker00@gmail.com>
2023-12-04 16:59:41 -0500

    youtube links updated

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -17,25 +17,23 @@ export default function Example() {
   //   },
   // };
 
-  //add https://www.youtube.com/embed/6y5i9zwnx0A
-
   return (
     <div className="mx-4 mt-4">
       <Link href="/" className="font-dawning text-4xl">
         Samuel Parker
       </Link>
       <div className="">
-        <div className="h-[400px] max-w-[800px] py-4">
+        <div className="h-[350px] max-w-[600px] py-4">
           <Iframe
             url="https://www.youtube.com/embed/6y5i9zwnx0A"
-            title="Mandolin Orange - Wildfire - 11/2/2016 - Paste Studios, New York, NY"
+            title="Autumn Leaves - Samuel (Sam) Parker - November 2023"
             className="w-full h-full"
           ></Iframe>
         </div>
-        <div className="h-[400px] max-w-[800px] py-4">
+        <div className="h-[350px] max-w-[600px] py-4">
           <Iframe
-            url="https://www.youtube.com/embed/6y5i9zwnx0A"
-            title="Mandolin Orange - Wildfire - 11/2/2016 - Paste Studios, New York, NY"
+            url="https://www.youtube.com/embed/26CmDp1ZSHI"
+            title="But Beautiful - Samuel (Sam) Parker - November 2023"
             className="w-full h-full"
           ></Iframe>
         </div>
